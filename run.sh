b35a9dab6b8f62a1d64d65afc59a00ba9dedcfd3
Louis Desplanche <louis.desplanche@edu.esiee.fr>
2021-06-16 16:25:51 +0200

    Update run conf

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build . -t docker_api
-docker run -d --restart=always -v /compose:/compose -v /var/run/docker.sock:/var/run/docker.sock -p 8082:8082 --name docker_api docker_api
+docker run -d --restart=always -v /etc/sr/compose:/etc/sr/compose -v /var/run/docker.sock:/var/run/docker.sock -p 8082:8082 --name docker_api docker_api
